b08f628d68bfbe0dde26646a55060d2e8c94780c
Savrasov Denis <dsavrasov@inno.tech>
2022-11-23 20:09:55 +0300

    Арифметические операции

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста" + text.length());
+        int firstNumber;
+        int secondNumber;
+        System.out.println("Введите первое число:");
+        firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        secondNumber = new Scanner(System.in).nextInt();
+
+        int plus = firstNumber + secondNumber;
+        int minus = firstNumber - secondNumber;
+        int mult = firstNumber * secondNumber;
+        double divide = firstNumber / secondNumber;
+
+        System.out.println("Сумма: " + plus);
+        System.out.println("Разность: " + minus);
+        System.out.println("Произведение: " + mult);
+        System.out.println("Частное: " + divide);
     }
 }
